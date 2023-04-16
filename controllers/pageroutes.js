52edf86330a98f4c5d180e4ca26246c59f2c1be5
William Kerby <w.kerby99@gmail.com>
2023-04-16 18:44:29 -0400

    save user attributed to post into a user variable and render onto single post handlebars view

--- controllers/pageroutes.js
+++ controllers/pageroutes.js
@@ -9,7 +9,6 @@ router.get("/", async (req, res) => {
     try {
         const allPosts = await Post.findAll({include:[{model:User}]});//query back all posts that have been created
         const posts = allPosts.map((post) => post.get({plain:true})); //serialize the post data
-        console.log(posts);
         res.status(200).render("allposts", {posts});
     }
 
@@ -29,16 +28,17 @@ router.get("/post/:id", async (req, res) => {
         const specPost = await Post.findByPk(specPostId, {include: [{model: Comment, include: [{model: User}]}]
         });
         const comment = specPost.comments.map((com) => com.get({plain:true})); //what is specPost.comments? Does JavaScript know what this is? Guessing that it is any comments associated with this specific post?
-        console.log(comment);
         const date = specPost.dataValues.date;
         const title = specPost.dataValues.title;
         const content = specPost.dataValues.content;
+        const specPostUser = await Post.findByPk(specPostId, {include:[{model:User}]});
+        const user = specPostUser.user.dataValues.username; //save the user who made the post into a variable and display in the handlebars view
         res.status(200).render("singlepost", {
             title,
             content,
             date,
             comment,
-
+            user
         })
 
     }
