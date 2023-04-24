5fad844fa1b70c340cc10db91e66ea23b078d5fa
William Kerby <w.kerby99@gmail.com>
2023-04-23 22:21:45 -0400

    add a js file for event listenet for add post button in user's dashboard view

---
+++ public/js/addpost.js
@@ -0,0 +1,24 @@
+const addButton = document.getElementById("newPost");
+
+addButton.addEventListener("click", async (e) => {
+    e.stopPropagation();
+    const title = document.getElementById("titleContent").value.trim();
+    const content = document.getElementById("contentContent").value.trim();
+    await fetch("api/newpost", {
+        method: "POST",
+        headers: {
+            Accept: "application/json, text/plain, */*", 
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            title: title,
+            content: content,
+          }),
+    }).then((res) => {
+        console.log(res);
+        if (res.status == 200) {
+            window.location.href = "/dashboard" //refresh the page with the new post in the user's dashboard if the post is successful
+        }
+        
+    })
+})
