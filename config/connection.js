77c0d7ed6cbb94174683763994dc2e231d73ab95
William Kerby <w.kerby99@gmail.com>
2023-04-29 14:22:17 -0400

    fix the connection.js file so that it incorporates jawsdburl and add a config.json file for connection

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,15 @@
 const Sequelize = require('sequelize');
 require('dotenv').config(); // allow parameters for database connection to be read in from the .env file
 
-const sequelize = new Sequelize(
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+}
+
+
+else {
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASS,
@@ -11,5 +19,7 @@ const sequelize = new Sequelize(
       port: 3306,
     }
   );
+}
+
   
   module.exports = sequelize;
