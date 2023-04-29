50d787656bcb1ae61315d4a91c3827b47d8c118d
William Kerby <w.kerby99@gmail.com>
2023-04-29 11:08:13 -0400

    require helper function in the server.js file

--- controllers/pageroutes.js
+++ controllers/pageroutes.js
@@ -34,7 +34,8 @@ router.get("/post/:id", async (req, res) => {
         const date = specPost.dataValues.date;
         const title = specPost.dataValues.title;
         const content = specPost.dataValues.content;
-        const loggedIn = req.session.loggedIn
+        const loggedIn = req.session.loggedIn;
+        const currentUser = req.session.currentUser;
         const specPostUser = await Post.findByPk(specPostId, {include:[{model:User}]});
         const user = specPostUser.user.dataValues.username; //save the user who made the post into a variable and display in the handlebars view
         res.status(200).render("singlepost", {
@@ -43,7 +44,8 @@ router.get("/post/:id", async (req, res) => {
             content,
             date,
             comment,
-            user
+            user,
+            currentUser,
         })
 
     }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const sequelize = require("./config/connection"); //require sequelize
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const exphbs = require('express-handlebars'); //require package for using handlebars
+const helpers = require("./utils/helpers");
 
 const routes = require("./controllers");
 const path = require('path');
